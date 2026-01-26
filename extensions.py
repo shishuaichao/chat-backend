4ba76cce892cc4bfcf3cbf4058f5239e0d2a964e
xiaoxia <shishuaichao578@163.com>
2026-01-26 20:58:35 +0800

    ws拆分

--- extensions.py
+++ extensions.py
@@ -3,11 +3,4 @@ from flask_socketio import SocketIO
 
 socketio = SocketIO()  # 暂不绑定app，仅作为事件注册的载体
 
-# def create_socketio(app):
-#     socketio = SocketIO(
-#         app,
-#         async_mode='eventlet',  # 核心：指定 eventlet 模式，不依赖 Werkzeug
-#         cors_allowed_origins="*"  # 允许跨域，生产环境指定具体域名
-#     )
-#     return socketio
 
